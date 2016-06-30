611178250e65bf51485b80d032fb307cc175e355
赵宝 <zhaobao@lubanec.com>
2016-06-30 17:41:28 +0800

    注册页面  信息完善页面

--- js/register.js
+++ js/register.js
@@ -23,74 +23,138 @@ function createCode() {
 
 }
 
-/*检测登录用户名*/
-function checkUsername(){
-    var userNameValue
-    userNameValue=$(".username").val()
-    if(userNameValue){
-        $(".usernameSpanY").removeClass("show").addClass("hide")
-    }else{
-        $(".usernameSpanY").removeClass("hide").addClass("show")
-    }
+/*登录用户名获取焦点*/
+function checkLoginUsername(){
+     $(".loginUserNameSpanY").removeClass("hide").addClass("show")
 }
-
-/*检测登录密码*/
+/*登录用户名失去焦点*/
+function checkLoginUsername1(){
+    $(".loginUserNameSpanY").removeClass("show").addClass("hide")
+}
+/*登录密码获得焦点*/
 function checkPassword(){
-    var passwordValue
-    passwordValue=$(".password").val()
-    if(passwordValue){
-        $(".passwordSpanN").removeClass("show").addClass("hide")
-    }else{
-        $(".passwordSpanN").removeClass("hide").addClass("show")
-    }
+    $(".passwordSpanN").removeClass("hide").addClass("show")
 }
-/*检测重复登录密码*/
+/*登录密码失去焦点*/
+function checkPassword1(){
+    $(".passwordSpanN").removeClass("show").addClass("hide")
+    $(".repasswordSpanY").removeClass("show").addClass("hide")
+}
+/*重复登录密码获取焦点*/
 function checkRepassword(){
-    var repasswordVal,passwordValue
-    repasswordVal=$(".repassword").val()
+    var passwordValue,repasswordVal
     passwordValue=$(".password").val()
-    if(repasswordVal){
-        $(".repasswordSpanY").removeClass("show").addClass("hide")
-        if(passwordValue==repasswordVal){
-            $(".repasswordSpanN").removeClass("hide").addClass("show")
-        }else{
-            $(".repasswordSpanN").removeClass("show").addClass("hide")
-        }
-    }else{
+    repasswordVal=$(".repassword").val()
+    if(passwordValue){
+        $(".repasswordSpanN").removeClass("show").addClass("hide")
         $(".repasswordSpanY").removeClass("hide").addClass("show")
-    }
-
-
-}
-/*检测用户名*/
-function checkName(){
-   var nameValue
-    nameValue=$(".NAME").val()
-    if(nameValue){
-        $(".NAMESpanN").removeClass("show").addClass("hide")
     }else{
-        $(".NAMESpanN").removeClass("hide").addClass("show")
+        $(".repasswordSpanY").removeClass("show").addClass("hide")
     }
 }
 
-/*检测手机*/
+/*重复登录密码失去焦点*/
+function checkRepassword1(){
+    var passwordValue,repasswordVal
+    passwordValue=$(".password").val()
+    repasswordVal=$(".repassword").val()
+    if(passwordValue){
+        if(repasswordVal){
+            $(".repasswordSpanY").removeClass("show").addClass("hide")
+            if(passwordValue==repasswordVal){
+                $(".repasswordSpanN").removeClass("show").addClass("hide")
+            }else{
+                $(".repasswordSpanN").removeClass("hide").addClass("show")
+            }
+        }else{
+            $(".repasswordSpanY").removeClass("hide").addClass("show")
+        }
+    }
+}
+/*用户名获取焦点*/
+function checkUserName(){
+    $(".userNamepanN").removeClass("hide").addClass("show")
+}
+
+/*用户名失去焦点*/
+function checkUserName1(){
+    $(".userNamepanN").removeClass("show").addClass("hide")
+}
+
+/*手机获取焦点*/
 function checkPhone(){
     var phoneValue
-    phoneValue=$(".PHONE").val()
+    phoneValue=$(".phone").val()
+    $(".phoneSpanN").removeClass("show").addClass("hide")
     if(phoneValue){
-        $(".PHONESpanN").removeClass("show").addClass("hide")
+        $(".phoneSpanY").removeClass("show").addClass("hide")
     }else{
-        $(".PHONESpanN").removeClass("hide").addClass("show")
+        $(".phoneSpanY").removeClass("hide").addClass("show")
+    }
+}
+/*手机失去焦点*/
+function checkPhone1(){
+    $(".phoneSpanY").removeClass("show").addClass("hide")
+    var phoneValue
+    phoneValue=$(".phone").val()
+    if(phoneValue){
+        if(!(/^1[3|4|5|7|8]\d{9}$/.test(phoneValue))) {
+            $(".phoneSpanN").removeClass("hide").addClass("show")
+        }else{
+            $(".phoneSpanN").removeClass("show").addClass("hide")
+        }
+     }
+}
+/*检测邮箱获取焦点*/
+function checkEmail(){
+    var emailValue
+    emailValue=$(".email").val()
+    if(emailValue){
+        $(".emailSpanY").removeClass("show").addClass("hide")
+    }else{
+        $(".emailSpanY").removeClass("hide").addClass("show")
+    }
+}
+/*失去焦点*/
+function checkEmail1(){
+    $(".emailSpanY").removeClass("show").addClass("hide")
+    var emailValue
+    emailValue=$(".email").val()
+    var format=/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*([,;][,;\s]*(\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)*)*$/;
+    if(emailValue){
+        if(!(format.test(emailValue))){
+            $(".emailSpanN").removeClass("hide").addClass("show")
+        }else{
+            $(".emailSpanN").removeClass("show").addClass("hide")
+        }
     }
 }
 
-/*检测邮箱*/
-function checkEmail(){
-    var emailValue
-    emailValue=$(".EMAIL").val()
-    if(emailValue){
-        $(".EMAILSpanN").removeClass("show").addClass("hide")
+/*修改同意阅读的css*/
+function checkRegister(){
+    if($(".registerSpan").hasClass("inputClick")){
+        $(".registerSpan").removeClass("inputClick").addClass("inputNoClick")
+        $("#register_check").removeAttr("checked")
     }else{
-        $(".EMAILSpanN").removeClass("hide").addClass("show")
+        $(".registerSpan").removeClass("inputNoClick").addClass("inputClick")
+        $("#register_check").prop("checked",true)
+    }
+}
+
+
+/*表单提交
+* 如果registerButton有registerNoButton，无法提交
+* */
+function formSubmit(){
+    var flag=false
+    //判断登录用户名
+    var loginUserName=$(".loginUserName").val()
+    //if()
+
+
+    if($(".registerButton").hasClass("registerNoButton")){
+
+    }else{
+        $("#userForm").submit()
     }
 }
